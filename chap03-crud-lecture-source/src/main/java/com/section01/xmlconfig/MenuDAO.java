b7175f99d3661b9a5e20b66b57c4bbc5779fcd0a
yunjiniius <yunjiniius@gmail.com>
2023-05-25 15:54:56 +0900

    2023-05-25 (05/24 휴강분 보강) lecture sources

---
+++ chap03-crud-lecture-source/src/main/java/com/section01/xmlconfig/MenuDAO.java
@@ -0,0 +1,38 @@
+package com.section01.xmlconfig;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class MenuDAO {
+
+    /* 해당 DAO에서 호출하는 이름으로는 menu-mapper.xml의 namespace에 있는 MenuMapper와 일치시켜야한다.
+     * section02, section03까지 구현시 MenuMapper 링크가 section02로 연결되지만 실제로 사용하는 매퍼는 menu-mapper.xml 이다.
+     * */
+
+    public List<MenuDTO> selectAllMenu(SqlSession sqlSession) {
+
+        return sqlSession.selectList("MenuMapper.selectAllMenu");
+    }
+
+    public MenuDTO selectMenuByCode(SqlSession sqlSession, int code) {
+
+        return sqlSession.selectOne("MenuMapper.selectMenuByCode", code);
+    }
+
+    public int insertMenu(SqlSession sqlSession, MenuDTO menu) {
+
+        return sqlSession.insert("MenuMapper.insertMenu", menu);
+    }
+
+    public int updateMenu(SqlSession sqlSession, MenuDTO menu) {
+
+        return sqlSession.update("MenuMapper.updateMenu", menu);
+    }
+
+    public int deleteMenu(SqlSession sqlSession, int code) {
+
+        return sqlSession.delete("MenuMapper.deleteMenu", code);
+    }
+
+}
