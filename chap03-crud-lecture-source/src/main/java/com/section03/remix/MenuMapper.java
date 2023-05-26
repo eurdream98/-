78b2abe9b383e158e971817e35234fa37492733f
yunjiniius <yunjiniius@gmail.com>
2023-05-26 13:54:55 +0900

    2023-05-26 Lecture sources

---
+++ chap03-crud-lecture-source/src/main/java/com/section03/remix/MenuMapper.java
@@ -0,0 +1,17 @@
+package com.section03.remix;
+
+import java.util.List;
+
+public interface MenuMapper {
+
+    List<MenuDTO> selectAllMenu();
+
+    MenuDTO selectMenuByCode(int code);
+
+    int insertMenu(MenuDTO menu);
+
+    int updateMenu(MenuDTO menu);
+
+    int deleteMenu(int code);
+
+}
