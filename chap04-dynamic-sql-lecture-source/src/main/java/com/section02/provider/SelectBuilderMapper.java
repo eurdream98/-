331750719771f4c1c05a38b74255f82ebfa21ef2
yunjiniius <yunjiniius@gmail.com>
2023-05-26 13:57:06 +0900

    2023-05-26 Lecture(pm) sources

---
+++ chap04-dynamic-sql-lecture-source/src/main/java/com/section02/provider/SelectBuilderMapper.java
@@ -0,0 +1,31 @@
+package com.section02.provider;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.SelectProvider;
+
+import com.common.MenuDTO;
+import com.common.SearchCriteria;
+
+public interface SelectBuilderMapper {
+
+    @Results(id="menuResultMap", value = {
+            @Result(id = true, property = "code", column = "MENU_CODE"),
+            @Result(property = "name", column = "MENU_NAME"),
+            @Result(property = "price", column = "MENU_PRICE"),
+            @Result(property = "categoryCode", column = "CATEGORY_CODE"),
+            @Result(property = "orderableStatus", column = "ORDERABLE_STATUS"),
+    })
+
+    @SelectProvider(type=SelectBuilderProvider.class, method="selectAllMenu")
+    List<MenuDTO> selectAllMenu();
+
+    @ResultMap("menuResultMap")
+    @SelectProvider(type=SelectBuilderProvider.class, method="searchMenuByCondition")
+    List<MenuDTO> searchMenuByCondition(SearchCriteria searchCriteria);
+
+}
+
---
+++ chap04-dynamic-sql-lecture-source/src/main/java/com/section02/provider/SelectBuilderService.java
@@ -0,0 +1,56 @@
+package com.section02.provider;
+
+import static com.common.Template.getSqlSession;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.common.MenuDTO;
+import com.common.SearchCriteria;
+import com.section01.xml.DynamicSqlMapper;
+
+public class SelectBuilderService {
+
+    private SelectBuilderMapper mapper;
+
+    public void testSimpleStatement() {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(SelectBuilderMapper.class);
+
+        List<MenuDTO> menuList = mapper.selectAllMenu();
+
+        if(menuList != null && menuList.size() > 0) {
+            for(MenuDTO menu : menuList) {
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다.");
+        }
+
+        sqlSession.close();
+
+    }
+
+    public void testDynamicStatement(SearchCriteria searchCriteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(SelectBuilderMapper.class);
+
+        List<MenuDTO> menuList = mapper.searchMenuByCondition(searchCriteria);
+
+        if(menuList != null && menuList.size() > 0) {
+            for(MenuDTO menu : menuList) {
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다.");
+        }
+
+        sqlSession.close();
+
+    }
+
+}
+
